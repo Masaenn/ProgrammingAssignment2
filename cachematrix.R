89f7a390aba46b70cde6e472c8136ae366bafe0b
maria saenz <maria@saenz.info>
2015-10-25 12:44:54 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function makes a list to set and get a matrix and its inverse in an intrinsic environment variable
+## The second function calculates the function and set its inverse.  If the inverse is already set, the cached value is used
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a matrix x, and exposes three methods to set/get x and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+cachedInv <- NULL ## initialises inverse
+  
+  ## set x in parent env with the desired value, if inverse is already set, ignors
+  set <- function(userValue = matrix()) {
+    x <<- userValue 
+    cachedInv <<- NULL
 
 }
 
 
-## Write a short comment describing this function
+## This function sets inverse variable in parent env to desired value and return the value as a convenience
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+setInverse <- function(invVal) {
+    cachedInv <<- invVal 
+    return(cachedInv)
+  }
+  
+  getInverse  <- function() cachedInv
+  list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
 }
+
